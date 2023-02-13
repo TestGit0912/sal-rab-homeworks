ad404478902125378b4efd755c764cf888410f4a
YNS <smirnov6400946@gmail.com>
2023-02-13 17:09:01 +0300

    task2.1_2.2_2.3Solution

--- task_2/solution.js
+++ task_2/solution.js
@@ -87,7 +87,7 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     let freeShipping;
 
-    freeShipping = shippingSum === 0;
+    freeShipping = shippingSum == 0;
 
     // Конец решения задачи №2.3.
 
