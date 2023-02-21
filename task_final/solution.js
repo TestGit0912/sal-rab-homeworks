027bd5af278604e4f49d94dd892869727fe8c2f9
YNS <smirnov6400946@gmail.com>
2023-02-21 13:08:52 +0300

    task_final_start

--- task_final/solution.js
+++ task_final/solution.js
@@ -49,9 +49,8 @@ function sendRequest(name, phone, address, goods, sum) {
     }
 
     data.order = {
-        address: `ул. ${address.street}, дом ${address.house}, 
-        ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`,
-        sum: sum,
+        address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`,
+        sum: sum
     }
 
     return JSON.stringify({data:data});
