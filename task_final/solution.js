cfd70b137c39e146c63ab96bf8c3850a82b772e8
YNS <smirnov6400946@gmail.com>
2023-02-21 11:30:25 +0300

    task_final_start

--- task_final/solution.js
+++ task_final/solution.js
@@ -57,7 +57,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     // data.client = `${name} ${phone}`;
 
-    let jsonData = JSON.stringify(data);
+    // let jsonData = JSON.stringify(data);
 
     return JSON.stringify({data: data});
 }
