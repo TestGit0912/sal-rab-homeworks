193b6b9581e5a71b2b4dad502b29e3edf391c4bb
YNS <smirnov6400946@gmail.com>
2023-02-21 10:53:38 +0300

    task_final_start

--- task_final/solution.js
+++ task_final/solution.js
@@ -9,11 +9,14 @@
 
 // cформированный в соответствии с правилами:
 // Объект data содержит все данные
+
 // В объекте data есть свойства:
 // - client - строка, имя клиента + телефон клиента;
+
 // - order - объект, содержащий данные о заказе:
 //     - address - строка с адресом доставки, записанным человекопонятным языком (как в примере)
 //     - sum - стоимость заказа с учетом скидок и доставки
+
 // - goods: массив объектов с информацией о позициях заказа:
 //     - title - название позиции
 //     - count - количество в заказе
@@ -39,17 +42,19 @@
 
 function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
-
     let countOfGoods = goods.length;
+    data.client = `${name}, ${phone}`;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = ` ул. ${address.street}, дом ${address.house}, 
+    подъезд ${address.entrance}, этаж ${address.floor}, кв. ${address.flat}`
 
-    data.client = 'Иван';
+    data.order.sum = sum;   // name + phone + address + goods +
+
+    // data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
 
